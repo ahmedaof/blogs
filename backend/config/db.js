629629aa586d5a2ec1ea5777bd68f79265649e0c
ahmedaof <49912597+ahmedaof@users.noreply.github.com>
2022-03-02 20:55:57 +0200

    mongoose connected

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
 
-const dbConnect  = mongoose.connect('mongodb://localhost/blogs',{
-    useNewUrlParser:true,
-    useCreateIndex:true
-}).then(()=>console.log("connected"))
+const dbConnect  = async() => {
+    try{
+   await mongoose.connect('mongodb://localhost/blogs',{
+        useNewUrlParser:true,
+    })
+    console.log("connected to Database")
+
+}catch(err){
+console.log(err);
+//exit with failer
+process.exit(1);
+}}
 
 module.exports = dbConnect ;
